1c5ec176cfad49f4d0ea28d3e8afb878ecd524a5
Minsoo Kim <ms214@ms214.kr>
2024-02-14 00:48:56 +0900

    time range check

--- commands/utility/죄인.js
+++ commands/utility/죄인.js
@@ -1,5 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits, GuildMemberManager, User, GuildMember, GuildMemberRoleManager} = require('discord.js');
-const { 죄인ID } = require('../../config.json');
+const { SlashCommandBuilder, PermissionFlagsBits} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,25 +16,29 @@ module.exports = {
     
     async execute(interaction){
         const admin = await interaction.guild.members.fetch(interaction.user.id);
-        const admin_role = await interaction.guild.roles.valueOf().find(x => x.name === "admin");
         console.log(admin.roles.valueOf());
         if(admin.roles.valueOf().find(x=>x.name === 'admin')){
             const target = interaction.options.getUser('target');
-            const time = interaction.options.getString('time') ?? '60';
-            await interaction.reply('<@' + target.id + '>은(는) '+ time +'분 동안 죄인입니다.');
-            //target을 죄인에 넣어두는 코드
-            const member = await interaction.guild.members.fetch(target.id);
-            console.log(member);
-            member.roles.add(죄인ID);
-            //member.addRole(chriminal_role);
-            setTimeout(()=>chriminal(member, interaction, target), time*1000*60);
+            let time = interaction.options.getString('time') ?? '60';
+            time *= 1;
+            if(time < 0 || time > Number.MAX_SAFE_INTEGER) interaction.reply("잘못된 숫자가 입력되었습니다.");
+            else if(isNaN(time)) interaction.reply("숫자를 입력해주세요");
+            else{
+                await interaction.reply('<@' + target.id + '>은(는) '+ time +'분 동안 죄인입니다.');
+                //target을 죄인에 넣어두는 코드
+                const member = await interaction.guild.members.fetch(target.id);
+                const chriminal_role = await interaction.guild.roles.valueOf().find(x=>x.name=== '제인');
+                console.log(member);
+                member.roles.add(chriminal_role);
+                setTimeout(()=>chriminal(member, interaction, target, chriminal_role), time*1000*60);
+            }
         }else{
             interaction.reply('해당 명령어에 대한 권한이 없습니다.');
         }
     },
 };
 
-function chriminal(member, interaction, target){
-    member.roles.remove(죄인ID);
+function chriminal(member, interaction, target, chriminal_role){
+    member.roles.remove(chriminal_role);
     interaction.channel.send('<@'+target.id+'>'+'은(는) 죄인에서 풀려났습니다.');
 }
