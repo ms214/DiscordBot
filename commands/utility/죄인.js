ae58724bb98d3c2b41aa41fd79ea190fb94391bd
Minsoo Kim <ms214@ms214.kr>
2024-02-13 23:57:36 +0900

    역할체크, 한글화, 봇이 유저태그
    admin 역할이 없는 사용자는 명령어 권한 없음 출력

--- commands/utility/죄인.js
+++ commands/utility/죄인.js
@@ -13,21 +13,29 @@ module.exports = {
     .addStringOption(option=>
         option
             .setName('time')
-            .setDescription('죄인에 넣어둘 시간'))
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+            .setDescription('죄인에 넣어둘 시간')),
     
     async execute(interaction){
-        const target = interaction.options.getUser('target');
-        const time = interaction.options.getString('time') ?? '60';
-        await interaction.reply(`Mute ${target.username} for ${time} minutes.`);
-        //target을 죄인에 넣어두는 코드
-        const chriminal_role = interaction.guild.roles.valueOf().find(x => x.name === "죄인");
-        //const member = interaction.guild.members.valueOf().get(target.id);
-        const member = await interaction.guild.members.fetch(target.id);
-        console.log(member);
-        member.roles.add(죄인ID);
-        //member.addRole(chriminal_role);
-        setTimeout(() => member.roles.remove(죄인ID) , time*1000*60);
-        interaction.channel.send('<@'+target.id+'>'+'은(는) 죄인에서 풀려났습니다.');
+        const admin = await interaction.guild.members.fetch(interaction.user.id);
+        const admin_role = await interaction.guild.roles.valueOf().find(x => x.name === "admin");
+        console.log(admin.roles.valueOf());
+        if(admin.roles.valueOf().find(x=>x.name === 'admin')){
+            const target = interaction.options.getUser('target');
+            const time = interaction.options.getString('time') ?? '60';
+            await interaction.reply('<@' + target.id + '>은(는) '+ time +'분 동안 죄인입니다.');
+            //target을 죄인에 넣어두는 코드
+            const member = await interaction.guild.members.fetch(target.id);
+            console.log(member);
+            member.roles.add(죄인ID);
+            //member.addRole(chriminal_role);
+            setTimeout(()=>chriminal(member, interaction, target), time*1000*60);
+        }else{
+            interaction.reply('해당 명령어에 대한 권한이 없습니다.');
+        }
     },
 };
+
+function chriminal(member, interaction, target){
+    member.roles.remove(죄인ID);
+    interaction.channel.send('<@'+target.id+'>'+'은(는) 죄인에서 풀려났습니다.');
+}
